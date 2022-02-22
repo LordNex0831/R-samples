e4762ea15ba903dc1b0de417705fe4261abd4c37
Mani Bhushan <bpvermamani@gmail.com>
2022-02-22 14:54:18 +0530

    22-02-2022: Adding comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,13 @@
+#Combining X_train and X_test
 X<-rbind(X_train,X_test)
+#Renaming the columns of X using the features dataset provided
 for(i in 1:ncol(X)){
   colnames(X)[i]=as.character(features[['V2']][[i]])
 }
+# CCombining y_train and y_test
 y<-rbind(y_train,y_test)
+#Renaming y column to "Result"
 colnames(y)<-"Result"
+#The combined dataset containig all X and y values
 data<-cbind(X,y)
+
