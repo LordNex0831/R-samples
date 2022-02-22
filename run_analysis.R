ff076b0899c21eefd64b355fe9835f36e89b3f8b
Mani Bhushan <bpvermamani@gmail.com>
2022-02-22 15:30:39 +0530

    22-02-2022: Created new frame containing mean and standard deviation of eact colunm of the combined dataset, except for the results

--- run_analysis.R
+++ run_analysis.R
@@ -10,3 +10,20 @@ y<-rbind(y_train,y_test)
 colnames(y)<-"Result"
 #The combined dataset containig all X and y values
 data<-cbind(X,y)
+
+#Creating empty frames to store feature name, mean and standard deviaiton for each column
+feature_data<-data.frame()
+mean_data<-data.frame()
+sd_data<-data.frame()
+#Calculating mean and standard deviation
+for(i in 1:ncol(X)){
+  feature_val<-data.frame(as.character(features[['V2']][[i]]))
+  mean_val<-data.frame(mean(X[[i]]))
+  sd_val<-data.frame(sd(X[[i]]))
+  feature_data<-rbind(feature_data,feature_val)
+  mean_data<-rbind(mean_data,mean_val)
+  sd_data<-rbind(sd_data,sd_val)
+}
+# Combining features,mean and standard data frames
+data2<-cbind(feature_data,mean_data,sd_data)
+colnames(data2)<-c('Feature_Description','Mean','Standard_deviation')
